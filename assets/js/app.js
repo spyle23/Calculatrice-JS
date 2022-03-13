69b26d960700287579e582ab983e39656ea13d46
spyle23 <sampingyen.lee@gmail.com>
2022-03-13 22:09:14 +0300

    finalisation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,21 @@
 /*------------------implémentation de la calculatrice en JS--------------------------*/
+
+/*----------------------------------------implémentation des boutons de bases---------------------------------*/
 let ecran = document.getElementById("ecran");       //on récupere dans un objet l'ecran ou le résultat s'affiche
 ecran.value = "0";
 
 let btnClic = false;    //le booleen permettant de voir si on doit ecraser la valeur sur l'ecran ou bien concatener la chaine
 let typeAct = " ";       //le type d'action à faire, operation +, -, x, /
 let arrayBouton = [];   //l'array qui contient tous les boutons
+let number1 = 0;
+let number2 = 0;
 
 //on boucle pour récuperer les boutons et on ajoute un ecouteur d'évènement pour faire une action précise à l'ecran
 for (let i = 0; i < 10; i++) {
     arrayBouton.push(document.getElementById("btn" + i));  
     arrayBouton[i].addEventListener("click", function (e) {
         if (btnClic) {
-            ecran.value = " "+ i;
+            ecran.value = ""+ i;
             btnClic = false;
         }
         else
@@ -30,6 +34,78 @@ arrayBouton[10].addEventListener("click",function (e) {     //quand ce bouton es
         ecran.value = ecran.value + ".";
     }
 });
+//lorsque l'utilisateur voudrais quitter le programme
+arrayBouton.push(document.getElementById("btnExit"));
+arrayBouton[11].addEventListener("click",function (e) {
+    //en cours d'implémentation
+});
 
-arrayBouton.push
+/*---------------------------------------------------implémentation des opérations------------------------------------------*/
+//pour l'addition
+arrayBouton.push(document.getElementById("btnAdd"));
+arrayBouton[12].addEventListener("click",function (e) {
+    number1 = ecran.value;
+    typeAct = "+";
+    btnClic = true;    
+});
+
+//pour la soustraction
+arrayBouton.push(document.getElementById("btnSous"));
+arrayBouton[13].addEventListener("click",function (e) {
+    number1 = ecran.value;
+    typeAct = "-";
+    btnClic = true;    
+});
+
+//pour la division
+arrayBouton.push(document.getElementById("btnDiv"));
+arrayBouton[14].addEventListener("click",function (e) {
+    number1 = ecran.value;
+    typeAct = "/";
+    btnClic = true;    
+});
+
+//pour la multiplication
+arrayBouton.push(document.getElementById("btnProd"));
+arrayBouton[15].addEventListener("click",function (e) {
+    number1 = ecran.value;
+    typeAct = "x";
+    btnClic = true;    
+});
+
+// pour la fonction reset
+arrayBouton.push(document.getElementById("btnReset"));
+arrayBouton[16].addEventListener("click",function (e) {
+    ecran.value = "0";    
+});
+
+//pour la fonction result
+arrayBouton.push(document.getElementById("btnResult"));
+arrayBouton[17].addEventListener("click",function (e) {
+    let result =0;
+    console.log(typeAct);
+    number2 = ecran.value;
+    switch (typeAct) {
+        case "+":
+            result = Number(number1) + Number(number2);
+            ecran.value = result;
+            break;
+        case "-":
+            result = number1 - number2;
+            ecran.value = result;
+            break;
+        case "/":
+            if (number2 == 0) {
+                alert("maths error");
+                return;
+            }
+            result = number1 / number2;
+            ecran.value = result;
+            break;
+        case "x":
+            result = number1 * number2;
+            ecran.value = result;
+            break;
+    }
+});
 
